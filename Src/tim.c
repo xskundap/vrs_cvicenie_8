629d391b29ba04be9196ab306bfd8fb2a0aea69f
Tvarožek Marian <91201706+xtvarozekm@users.noreply.github.com>
2021-12-13 17:56:29 +0100

    Update tim.c

--- Src/tim.c
+++ Src/tim.c
@@ -40,7 +40,7 @@ void MX_TIM2_Init(void)
   //TIM2 clock = 8MHz
   TIM_InitStruct.Prescaler = 7999; 								//1kHz
   TIM_InitStruct.CounterMode = LL_TIM_COUNTERMODE_UP;
-  TIM_InitStruct.Autoreload = 499;								//20ms
+  TIM_InitStruct.Autoreload = 499;								//500ms
   TIM_InitStruct.ClockDivision = LL_TIM_CLOCKDIVISION_DIV1;
   LL_TIM_Init(TIM2, &TIM_InitStruct);
 
